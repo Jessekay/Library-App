3ae43631d0f173feeba6017478d9a1fa560ac9d1
Jessekay <kayjes00@gmail.com>
2024-09-16 14:53:41 +0200

    add a new book button for adding  a new book to the library, also used dialog tag for the form

--- index.js
+++ index.js
@@ -35,7 +35,43 @@ function displayBooks() {
   });
 }
 
+// Show Book Form Modal
+const newBookButton = document.getElementById('newBookBtn');
+const bookFormDialog = document.getElementById('bookFormDialog');
+newBookButton.addEventListener('click', () => {
+  bookFormDialog.showModal();
+});
+
+// Close Modal and Cancel Button
+const cancelButton = document.getElementById('cancelButton');
+cancelButton.addEventListener('click', () => {
+  bookFormDialog.close();
+});
+
+// Book Submission
+const bookForm = document.getElementById('bookForm');
+bookForm.addEventListener('submit', (event) => {
+  event.preventDefault(); // Prevent from refreshing the page
+
+  // Get form input values
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  const pages = document.getElementById('pages').value;
+  const isRead = document.getElementById('isRead').checked;
+
+  // Add the new book to the library
+  addBookToLibrary(title, author, pages, isRead);
+
+  // Close the model and reset the form
+  bookFormDialog.close();
+  bookForm.reset();
+
+displayBooks();
+
+});
+
+
 addBookToLibrary('The Great Gatsby', 'F. Scott Fitzgerald', 180, false);
 addBookToLibrary('1984', 'George Orwell', 328, true);
 
-console.log(displayBooks());
+displayBooks();
